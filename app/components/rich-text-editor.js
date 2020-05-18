a8f7c6b0db3dc5ab897f1d03d733275589709690
Sarah Harrington <sarah.harrington@iorahealth.com>
2020-05-18 16:52:15 -0500

    working on adding ckeditor

--- app/components/rich-text-editor.js
+++ app/components/rich-text-editor.js
@@ -1,4 +1,27 @@
-import Component from '@glimmer/component';
+import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
-export default class RichTextEditorComponent extends Component {
+export default class TextEditorComponent extends Component {
+  @tracked editor;
+  @tracked textBody = "";
+
+  @action
+  loadEditor() {
+    ClassicEditor.create(document.querySelector("#editor-container"))
+      .then((newEditor) => {
+        this.editor = newEditor;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  @action
+  updatedBody() {
+    this.textBody = this.editor.getData();
+    console.log(this.textBody);
+    this.args.bodyUpdated(this.textBody);
+  }
 }
